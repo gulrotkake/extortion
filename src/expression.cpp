1a25e7777a335d44a6699e31dcbd865514aea589
Michael Mortensen <michael@surtr.(none)>
2011-01-12 01:28:31 +0100

    Rewriting the parser, just storing the new incomplete version as a comment until I get around to fixing the details.

--- src/expression.cpp
+++ src/expression.cpp
@@ -1,3 +1,81 @@
+/* The actual grammar is soon to be replaced by:
+
+namespace extortion { namespace detail {
+
+struct attribute {
+    std::string name;
+    optional_string value;
+};
+
+struct component;
+
+typedef boost::variant<std::vector<component>, attribute> sub_expression;
+
+struct component {
+    component() {}
+
+    component(const std::string& name) {
+    }
+
+    component(
+        const std::string& name,
+        const std::vector<sub_expression>& sub_expressions
+    ) {
+    }
+};
+
+}}
+
+BOOST_FUSION_ADAPT_STRUCT(
+    extortion::detail::attribute,
+    (std::string, name)
+    (extortion::detail::optional_string, value)
+);
+
+namespace extortion { namespace detail {
+
+namespace qi = boost::spirit::qi;
+template <typename Iterator>
+struct path : qi::grammar<Iterator, std::vector<component>()> {
+    // boost::spirit::qi::rule<Iterator, expression()> expression_r;
+    boost::spirit::qi::rule<Iterator, std::vector<component>()> expression_r;
+    qi::rule<Iterator, component()> component_r;
+    // boost::spirit::qi::rule<Iterator, tuple()> tuple_r;
+    // boost::spirit::qi::rule<Iterator, pairs_type()> attributes_r;
+    qi::rule<Iterator, std::vector<sub_expression>()> sub_expressions_r;
+    qi::rule<Iterator, sub_expression()> sub_expression_r;
+    qi::rule<Iterator, attribute()> attribute_r;
+    qi::rule<Iterator, std::string()> key_r, value_r;
+    qi::rule<Iterator, boost::variant<std::vector<component>, attribute>()> foo_r;
+    path()
+        : path::base_type(expression_r)
+    {
+        namespace qi = boost::spirit::qi;
+
+        // expression =  "/" , component , { "/" , component } , [ "/" ]
+        expression_r = +(qi::omit['/'] >> component_r) >> qi::omit[-qi::char_('/')];
+        // component  = key , [ "[" , sub_expressions , "]" ]
+        component_r = key_r >> -('[' >> sub_expressions_r >> ']');
+
+        // sub_expressionss = subexpr , { '&' , subexpr }
+        sub_expressions_r = sub_expression_r % '&';
+
+        // sub_expression = expression_r | attribute_r
+        sub_expression_r = expression_r | attribute_r;
+
+        // attribute  = "@" , key , [ "=" , value ]
+        attribute_r   = '@' >> key_r >> -('=' >> value_r);
+
+        // key        = alpha , { alnum }
+        key_r         = qi::char_("a-zA-Z_") >> *qi::char_("a-zA-Z_0-9");
+
+        // value      = "'" , data , "'"
+        value_r       = '\'' >> *(qi::char_ -qi::char_('\'')) >> '\'';
+    }
+};
+
+}}
+*/
 #include "expression.hpp"
 #include <boost/spirit/include/qi.hpp>
 #include <boost/fusion/include/std_pair.hpp>
